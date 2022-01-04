17dc62ebb942903d5754ac1e31ea31e46fc0e4cf
myuksektepe <yuksektepemurat@gmail.com>
2022-01-04 13:31:02 +0300

    first commit

---
+++ app/src/main/java/murat/tfkb/MainActivity.kt
@@ -0,0 +1,11 @@
+package murat.tfkb
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
