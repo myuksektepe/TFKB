3f37d3061c40cf489128ce86d83d2d8340a52621
myuksektepe <yuksektepemurat@gmail.com>
2022-01-06 02:32:47 +0300

    edit

--- app/src/main/java/murat/tfkb/presentation/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/murat/tfkb/presentation/viewmodel/MainActivityViewModel.kt
@@ -5,14 +5,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import murat.tfkb.domain.model.ResultState
 import murat.tfkb.domain.model.SearchResultItem
-import murat.tfkb.domain.model.SearchResults
 import murat.tfkb.domain.use_case.GetSearchResult
 import javax.inject.Inject
 
@@ -24,15 +21,16 @@ class MainActivityViewModel @Inject constructor(
     private var searchJob: Job? = null
 
     private val _allResults = MutableLiveData<ResultState<MutableList<SearchResultItem>>>()
-    val allResults : LiveData<ResultState<MutableList<SearchResultItem>>>
+    val allResults: LiveData<ResultState<MutableList<SearchResultItem>>>
         get() = _allResults
 
     fun fetchAllResults(term: String, limit: Int, entity: String) {
-        searchJob?.cancel()
-        searchJob = viewModelScope.launch(Dispatchers.IO) {
+        //searchJob?.cancel()
+        //searchJob =
+        viewModelScope.launch {
 
             getSearchResult(term, limit, entity).collectLatest {
-                _allResults.postValue(it)
+                _allResults.value = it
             }
 
         }
